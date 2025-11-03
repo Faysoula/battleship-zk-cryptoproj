f3f68aae03ec3a2b775ae3b9bb7a661b25ddd8f6
Faysoula <faysaljunior.elestwani@gmail.com>
2025-11-03 22:05:22 +0200

    Fix methods.rs to use relative paths

--- prebuilt/methods.rs
+++ prebuilt/methods.rs
@@ -1,6 +1,4 @@
-pub const INIT_ELF: &[u8] = include_bytes!("/home/soula/battleship-zk/target/riscv-guest/battleship-guests/battleship-methods/riscv32im-risc0-zkvm-elf/release/init.bin");
-pub const INIT_PATH: &str = "/home/soula/battleship-zk/target/riscv-guest/battleship-guests/battleship-methods/riscv32im-risc0-zkvm-elf/release/init.bin";
+pub const INIT_ELF: &[u8] = include_bytes!("init.bin");
 pub const INIT_ID: [u32; 8] = [1094500067, 3753570540, 584863037, 4268781899, 3346525540, 561062845, 1738496575, 1943770622];
-pub const ROUND_ELF: &[u8] = include_bytes!("/home/soula/battleship-zk/target/riscv-guest/battleship-guests/battleship-methods/riscv32im-risc0-zkvm-elf/release/round.bin");
-pub const ROUND_PATH: &str = "/home/soula/battleship-zk/target/riscv-guest/battleship-guests/battleship-methods/riscv32im-risc0-zkvm-elf/release/round.bin";
+pub const ROUND_ELF: &[u8] = include_bytes!("round.bin");
 pub const ROUND_ID: [u32; 8] = [2599187791, 203479244, 1117384671, 1723217785, 2689348825, 901924105, 4283015301, 2327131669];
